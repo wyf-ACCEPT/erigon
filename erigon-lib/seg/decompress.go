7932859eb1a3e5c38535d8b7500f0d4a8e7b38eb
alex.sharov <AskAlexSharov@gmail.com>
2024-04-17 10:07:22 +0700

    better logging

--- erigon-lib/seg/decompress.go
+++ erigon-lib/seg/decompress.go
@@ -329,11 +329,11 @@ func NewDecompressor(compressedFilePath string) (*Decompressor, error) {
 		}
 	}
 	d.wordsStart = pos + dictSize
-	fmt.Printf("[dbg] %d, %d, %d, %d\n", d.Count(), dictSize, d.size, compressedMinSize)
-	if d.Count() == 0 && dictSize == 0 && d.size > compressedMinSize {
-		return nil, &ErrCompressedFileCorrupted{
-			FileName: fName, Reason: fmt.Sprintf("size %v but no words in it", datasize.ByteSize(d.size).HR())}
-	}
+
+	//if d.Count() == 0 && dictSize == 0 && d.size > compressedMinSize {
+	//	return nil, &ErrCompressedFileCorrupted{
+	//		FileName: fName, Reason: fmt.Sprintf("size %v but no words in it", datasize.ByteSize(d.size).HR())}
+	//}
 	closeDecompressor = false
 	return d, nil
 }
