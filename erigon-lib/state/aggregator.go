3477c28e0eab188472fbc9bd98db323bf50c1bc7
alex.sharov <AskAlexSharov@gmail.com>
2024-06-05 11:53:06 +0700

    save

--- erigon-lib/state/aggregator.go
+++ erigon-lib/state/aggregator.go
@@ -786,12 +786,12 @@ func (ac *AggregatorRoTx) CanUnwindBeforeBlockNum(blockNum uint64, tx kv.Tx) (ui
 	if err != nil {
 		_minBlockNum, _ := ac.MinUnwindDomainsBlockNum(tx)
 		log.Warn("[dbg] see1", "_minBlockNum", _minBlockNum)
-		return _minBlockNum, blockNum < _minBlockNum, nil //nolint
+		return _minBlockNum, blockNum >= _minBlockNum, nil //nolint
 	}
 	if blockNumWithCommitment == 0 && ac.CanUnwindDomainsToTxNum() > 0 { // don't allow unwind beyond files progress
 		_minBlockNum, _ := ac.MinUnwindDomainsBlockNum(tx)
 		log.Warn("[dbg] see3", "_minBlockNum", _minBlockNum)
-		return _minBlockNum, blockNum < _minBlockNum, nil
+		return _minBlockNum, blockNum >= _minBlockNum, nil
 	}
 	log.Warn("[dbg] see2", "blockNumWithCommitment", blockNumWithCommitment)
 	return blockNumWithCommitment, true, nil
