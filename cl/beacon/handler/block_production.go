7e1b042aa1e0ea70ec6898d3dfbc8ed98108a5d3
kewei <kewei.train@gmail.com>
2024-06-11 01:39:27 +0800

    update

--- cl/beacon/handler/block_production.go
+++ cl/beacon/handler/block_production.go
@@ -194,11 +194,14 @@ func (a *ApiHandler) GetEthV3ValidatorBlock(
 				Slot:          block.Slot,
 				ProposerIndex: block.ProposerIndex,
 				ParentRoot:    block.ParentRoot,
-				StateRoot:     block.StateRoot,
 				Body:          block.BeaconBody,
 			},
 		}
 		if err := machine.ProcessBlock(transition.DefaultMachine, baseState, signedBeaconBlock); err != nil {
+			return nil, err
+		}
+		block.StateRoot, err = baseState.HashSSZ()
+		if err != nil {
 			return nil, err
 		}
 	}
@@ -271,10 +274,6 @@ func (a *ApiHandler) produceBlock(
 		return nil, localErr
 	}
 	// prepare basic block
-	stateRoot, err := baseState.HashSSZ()
-	if err != nil {
-		return nil, err
-	}
 	proposerIndex, err := baseState.GetBeaconProposerIndex()
 	if err != nil {
 		return nil, err
@@ -287,7 +286,6 @@ func (a *ApiHandler) produceBlock(
 		Slot:          targetSlot,
 		ProposerIndex: proposerIndex,
 		ParentRoot:    baseBlockRoot,
-		StateRoot:     stateRoot,
 	}
 	if !a.routerCfg.Builder || builderErr != nil {
 		// directly return the block if:
