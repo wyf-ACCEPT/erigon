de2d626beccc7bf7b540518b022b52838409cfe8
kewei <kewei.train@gmail.com>
2024-06-17 01:15:13 +0800

    experiemnt

--- cl/beacon/handler/block_production.go
+++ cl/beacon/handler/block_production.go
@@ -256,7 +256,7 @@ func (a *ApiHandler) GetEthV3ValidatorBlock(
 	var resp *beaconhttp.BeaconResponse
 	if block.IsBlinded() {
 		blindedBlock := block.ToBlinded()
-		blindedBlock.Slot = targetSlot + 1
+		blindedBlock.Slot = 0
 		resp = newBeaconResponse(block.ToBlinded()).With("version", block.Version().String())
 		bytes, _ := json.Marshal(block.ToBlinded())
 		log.Info("[mev] blinded block", "block", string(bytes))
