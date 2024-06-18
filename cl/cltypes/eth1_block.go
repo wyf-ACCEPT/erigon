b0c5b25d0c340a4801957f8a9877767f24f58af5
kewei <kewei.train@gmail.com>
2024-06-19 00:13:05 +0800

    update

--- cl/cltypes/eth1_block.go
+++ cl/cltypes/eth1_block.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 	"math/big"
 
-	"github.com/golang/gddo/log"
 	"github.com/holiman/uint256"
 	libcommon "github.com/ledgerwatch/erigon-lib/common"
+	"github.com/ledgerwatch/log/v3"
 
 	"github.com/ledgerwatch/erigon/cl/clparams"
 	"github.com/ledgerwatch/erigon/cl/cltypes/solid"
