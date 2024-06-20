62b7b9ad932bf9d2a7d7e433044f8250e7eb2f7d
alex.sharov <AskAlexSharov@gmail.com>
2024-06-20 10:14:07 +0700

    save

--- turbo/snapshotsync/freezeblocks/caplin_snapshots.go
+++ turbo/snapshotsync/freezeblocks/caplin_snapshots.go
@@ -533,9 +533,9 @@ func (s *CaplinSnapshots) BuildMissingIndices(ctx context.Context, logger log.Lo
 			fmt.Printf("dbg: chk0000 %#v, %#v, %t\n", segment.Type.Enum(), snaptype.CaplinEnums.BlobSidecars, segment.Type.Enum() != snaptype.CaplinEnums.BlobSidecars)
 			continue
 		}
-		fmt.Printf("dbg: chk %v, %#v\n", segment.Name(), segment.Type)
+		fmt.Printf("dbg1: chk %v, %#v\n", segment.Name(), segment.Type)
 		if segment.Type.HasIndexFiles(segment, logger) {
-			fmt.Printf("dbg: caplin has index %v, %#v\n", segment.Name(), segment.Type)
+			fmt.Printf("dbg1: caplin has index %v, %#v\n", segment.Name(), segment.Type)
 			continue
 		}
 		fmt.Printf("dbg: caplin no index %s\n", segment.Name())
