1cd002771b51263ffe82e6e2664d9063933f0733
alex.sharov <AskAlexSharov@gmail.com>
2024-06-20 10:06:23 +0700

    save

--- erigon-lib/downloader/snaptype/type.go
+++ erigon-lib/downloader/snaptype/type.go
@@ -244,6 +244,7 @@ func (s snapType) BuildIndexes(ctx context.Context, info FileInfo, chainConfig *
 }
 
 func (s snapType) HasIndexFiles(info FileInfo, logger log.Logger) bool {
+	fmt.Printf("alex: %s, %#v\n", s.name, s.indexes)
 	for _, index := range s.indexes {
 		if !index.HasFile(info, logger) {
 			return false
