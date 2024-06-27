ef01d0b41dcef5b07ca819df332904aa2b71c354
alex.sharov <AskAlexSharov@gmail.com>
2024-06-27 14:19:05 +0700

    save

--- cmd/integration/commands/reset_state.go
+++ cmd/integration/commands/reset_state.go
@@ -119,16 +119,18 @@ func printStages(tx kv.Tx, snapshots *freezeblocks.RoSnapshots, borSn *freezeblo
 			at.Close()
 			r.RunOptimize()
 			fmt.Printf("r: %s: %dMil, %dmb, bytes/key=%f\n", i, r.GetCardinality()/1_000_000, r.GetSerializedSizeInBytes()/1024/1024, float64(r.GetSerializedSizeInBytes())/float64(r.GetCardinality()))
-			ef := eliasfano32.NewEliasFano(r.GetCardinality(), r.Maximum())
-			iterR := r.Iterator()
-			for iterR.HasNext() {
-				ef.AddOffset(iterR.Next())
-			}
-			ef.Build()
+			if r.GetCardinality() > 0 {
+				ef := eliasfano32.NewEliasFano(r.GetCardinality(), r.Maximum())
+				iterR := r.Iterator()
+				for iterR.HasNext() {
+					ef.AddOffset(iterR.Next())
+				}
+				ef.Build()
 
-			var buf []byte
-			buf = ef.AppendBytes(buf[:0])
-			fmt.Printf("ef: %s: %dMil, %dmb, bytes/key=%f\n", i, r.GetCardinality()/1_000_000, len(buf), float64(len(buf))/float64(r.GetCardinality()))
+				var buf []byte
+				buf = ef.AppendBytes(buf[:0])
+				fmt.Printf("ef: %s: %dMil, %dmb, bytes/key=%f\n", i, r.GetCardinality()/1_000_000, len(buf), float64(len(buf))/float64(r.GetCardinality()))
+			}
 		}
 
 		panic(1)
