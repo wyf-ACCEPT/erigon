daa79da74cd9884e4fbe47cf8788eea46932d2f3
alex.sharov <AskAlexSharov@gmail.com>
2024-07-30 15:09:50 +0700

    save

--- eth/stagedsync/exec3.go
+++ eth/stagedsync/exec3.go
@@ -897,6 +897,7 @@ Loop:
 				if !skipPostEvaluation && (rs.SizeEstimate() < commitThreshold || inMemExec) && !aggregatorRo.CanPrune(applyTx, outputTxNum.Load()) {
 					break
 				}
+				fmt.Printf("[dbg] alex2: %d, %t\n", commitThreshold, aggregatorRo.CanPrune(applyTx, outputTxNum.Load()))
 				var (
 					commitStart = time.Now()
 					tt          = time.Now()
