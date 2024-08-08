f3c837f43f1161ff6831ab5b970c19646db6d873
alex.sharov <AskAlexSharov@gmail.com>
2024-08-08 18:50:24 +0700

    save

--- erigon-lib/state/inverted_index.go
+++ erigon-lib/state/inverted_index.go
@@ -576,11 +576,11 @@ func (iit *InvertedIndexRoTx) seekInFiles(key []byte, txNum uint64) (found bool,
 	}
 
 	hi, lo := iit.hashKey(key)
-	const limit = 128
 
+	const limit = 256
 	if iit.iiNotFoundCache == nil {
 		var err error
-		iit.iiNotFoundCache, err = freelru.New[uint64, uint64](4096, u64noHash)
+		iit.iiNotFoundCache, err = freelru.New[uint64, uint64](limit, u64noHash)
 		if err != nil {
 			panic(err)
 		}
@@ -591,7 +591,7 @@ func (iit *InvertedIndexRoTx) seekInFiles(key []byte, txNum uint64) (found bool,
 		hit++
 		//if dbg.KVReadLevelledMetrics {
 		m := iit.iiNotFoundCache.Metrics()
-		if hit%100_000 == 0 {
+		if hit%1_000_000 == 0 {
 			log.Warn("[dbg] lEachCache", "a", iit.ii.filenameBase, "hit", hit, "total", hit+miss, "Collisions", m.Collisions, "Evictions", m.Evictions, "Inserts", m.Inserts, "limit", limit, "ratio", fmt.Sprintf("%.2f", float64(m.Hits)/float64(m.Hits+m.Misses)))
 		}
 		//}
