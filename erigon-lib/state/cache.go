076affbd8920f820e486aaeeda744ad0fed69409
alex.sharov <AskAlexSharov@gmail.com>
2024-08-13 12:16:39 +0700

    save

--- erigon-lib/state/cache.go
+++ erigon-lib/state/cache.go
@@ -21,12 +21,29 @@ type domainGetFromFileCacheItem struct {
 	v   []byte // pointer to `mmap` - if .kv file is not compressed
 }
 
-var latestStateCachePerDomain = uint32(dbg.EnvInt("D_LRU", 128))
+var domainGetFromFileCacheLimit = uint32(dbg.EnvInt("D_LRU", 128))
 
 func NewDomainGetFromFileCache() *DomainGetFromFileCache {
-	c, err := freelru.New[u128, domainGetFromFileCacheItem](latestStateCachePerDomain, u128noHash)
+	c, err := freelru.New[u128, domainGetFromFileCacheItem](domainGetFromFileCacheLimit, u128noHash)
 	if err != nil {
 		panic(err)
 	}
 	return &DomainGetFromFileCache{c}
+}
+
+var iiGetFromFileCacheLimit = uint32(dbg.EnvInt("II_LRU", 512))
+
+type IISeekInFilesCache struct {
+	*freelru.LRU[uint64, iiSeekInFilesCacheItem]
+}
+type iiSeekInFilesCacheItem struct {
+	requested, found uint64
+}
+
+func NewIISeekInFilesCache() *IISeekInFilesCache {
+	c, err := freelru.New[uint64, iiSeekInFilesCacheItem](iiGetFromFileCacheLimit, u64noHash)
+	if err != nil {
+		panic(err)
+	}
+	return &IISeekInFilesCache{c}
 }
