44b4006b37282ab06c251eef80f8fd044ed1162d
Mark Holt <mark@distributed.vision>
2024-08-26 16:18:09 +0100

    add block no

--- turbo/snapshotsync/freezeblocks/block_reader.go
+++ turbo/snapshotsync/freezeblocks/block_reader.go
@@ -1404,7 +1404,7 @@ func (r *BlockReader) BorStartEventID(ctx context.Context, tx kv.Tx, hash common
 }
 
 func (r *BlockReader) EventsByBlock(ctx context.Context, tx kv.Tx, hash common.Hash, blockHeight uint64) ([]rlp.RawValue, error) {
-	fmt.Println("EventsByBlock")
+	fmt.Println("EventsByBlock", blockHeight)
 	maxBlockNumInFiles := r.FrozenBorBlocks()
 	if tx != nil && (maxBlockNumInFiles == 0 || blockHeight > maxBlockNumInFiles) {
 		c, err := tx.Cursor(kv.BorEventNums)
