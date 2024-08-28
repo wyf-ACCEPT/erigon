fe2bb7165832cb0d999a305820edb6ca356f6dda
alex.sharov <AskAlexSharov@gmail.com>
2024-08-28 15:49:28 +0700

    save

--- eth/stagedsync/exec3.go
+++ eth/stagedsync/exec3.go
@@ -1155,6 +1155,7 @@ func flushAndCheckCommitmentV3(ctx context.Context, header *types.Header, applyT
 
 	if bytes.Equal(rh, header.Root.Bytes()) {
 		if !inMemExec {
+			log.Warn("[dbg] flush trie", "bn", doms.BlockNum())
 			if err := doms.Flush(ctx, applyTx); err != nil {
 				return false, err
 			}
